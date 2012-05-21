08e1e47050711749e2b35fc3336eb547610c3597
Ma Yingkui <maeikei@gmail.com>
2012-05-21 12:23:34 +0900

    q

--- clang.define.cmake
+++ clang.define.cmake
@@ -1,11 +1,11 @@
-
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} /opt/llvm/share/llvm/cmake)
-#include(${LLVM_TOP}/share/llvm/cmake/LLVMProcessSources.cmake)
-include(AddLLVM)
-#find_package(LLVM)
-
-macro(add_clang_executable name)
-  add_llvm_executable( ${name} ${ARGN} )
-  set_target_properties(${name} PROPERTIES FOLDER "Clang executables")
+set(LLVM_TOP_DIR /opt/llvm)
+include_directories(${LLVM_TOP_DIR}/include)
+add_definitions(
+	-D__STDC_LIMIT_MACROS
+	-D__STDC_CONSTANT_MACROS
+)
+macro(add_xclang_executable name)
+  add_executable( ${name} ${ARGN} )
+  set_target_properties(${name} PROPERTIES FOLDER "Xclang executables")
 endmacro(add_clang_executable)
 
