ed757a0a7384f62c1d0f12946681e32597fcc73f
Ma Yingkui <maeikei@gmail.com>
2012-05-23 23:54:05 +0900

    test commit

--- clang.define.cmake
+++ clang.define.cmake
@@ -4,8 +4,39 @@ add_definitions(
 	-D__STDC_LIMIT_MACROS
 	-D__STDC_CONSTANT_MACROS
 )
+link_directories(${LLVM_TOP_DIR}/lib)
 macro(add_xclang_executable name)
   add_executable( ${name} ${ARGN} )
+  target_link_libraries( ${name}
+  LLVMAsmParser
+  LLVMBitReader
+  LLVMBitWriter
+  LLVMCodeGen
+  LLVMInstrumentation
+  LLVMipo
+  LLVMLinker
+  LLVMSelectionDAG
+
+  clangFrontendTool
+  clangAST
+  clangAnalysis
+  clangBasic
+  clangCodeGen
+  clangDriver
+  clangEdit
+  clangFrontend
+  clangLex
+  clangParse
+  clangEdit
+  clangARCMigrate
+  clangRewrite
+  clangSema
+  clangSerialization
+  clangStaticAnalyzerFrontend
+  clangStaticAnalyzerCheckers
+  clangStaticAnalyzerCore
+   
+   )
   set_target_properties(${name} PROPERTIES FOLDER "Xclang executables")
-endmacro(add_clang_executable)
+endmacro(add_xclang_executable)
 
