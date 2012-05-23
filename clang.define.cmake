b18a0a99e708fabfaa4e6c99a0baf30cb9d92779
Ma Yingkui <maeikei@gmail.com>
2012-05-24 00:14:47 +0900

    test commit

--- clang.define.cmake
+++ clang.define.cmake
@@ -8,34 +8,30 @@ link_directories(${LLVM_TOP_DIR}/lib)
 macro(add_xclang_executable name)
   add_executable( ${name} ${ARGN} )
   target_link_libraries( ${name}
-  LLVMAsmParser
-  LLVMBitReader
-  LLVMBitWriter
-  LLVMCodeGen
-  LLVMInstrumentation
-  LLVMipo
-  LLVMLinker
-  LLVMSelectionDAG
-
-  clangFrontendTool
-  clangAST
-  clangAnalysis
-  clangBasic
-  clangCodeGen
-  clangDriver
-  clangEdit
-  clangFrontend
-  clangLex
-  clangParse
-  clangEdit
-  clangARCMigrate
-  clangRewrite
-  clangSema
-  clangSerialization
-  clangStaticAnalyzerFrontend
-  clangStaticAnalyzerCheckers
-  clangStaticAnalyzerCore
-   
+## llvm clang --libs
+clangFrontendTool clangAST clangAnalysis clangBasic clangCodeGen 
+clangDriver clangEdit clangFrontend clangLex clangParse clangEdit 
+clangARCMigrate clangRewrite clangSema clangSerialization clangStaticAnalyzerFrontend 
+clangStaticAnalyzerCheckers clangStaticAnalyzerCore LLVMARMCodeGen LLVMARMAsmParser 
+LLVMARMDisassembler LLVMCellSPUCodeGen LLVMCppBackendCodeGen LLVMHexagonCodeGen 
+LLVMMipsCodeGen LLVMMipsAsmParser LLVMMipsDisassembler LLVMMBlazeCodeGen 
+LLVMMBlazeAsmParser LLVMMBlazeDisassembler LLVMMSP430CodeGen LLVMNVPTXCodeGen 
+LLVMPowerPCCodeGen LLVMPTXCodeGen LLVMSparcCodeGen LLVMX86CodeGen LLVMX86AsmParser 
+LLVMX86Disassembler LLVMXCoreCodeGen LLVMAsmParser LLVMBitWriter LLVMInstrumentation 
+LLVMipo LLVMLinker LLVMARMDesc LLVMCellSPUDesc LLVMCppBackendInfo LLVMHexagonAsmPrinter 
+LLVMHexagonDesc LLVMMipsDesc LLVMMBlazeDesc LLVMMSP430Desc LLVMNVPTXDesc LLVMPowerPCDesc 
+LLVMPTXDesc LLVMSparcDesc LLVMX86Desc LLVMAsmPrinter LLVMSelectionDAG LLVMXCoreDesc 
+LLVMVectorize LLVMArchive LLVMARMAsmPrinter LLVMARMInfo LLVMCellSPUInfo 
+LLVMHexagonInfo LLVMMipsAsmPrinter LLVMMipsInfo LLVMMBlazeAsmPrinter 
+LLVMMBlazeInfo LLVMMSP430AsmPrinter LLVMMSP430Info LLVMNVPTXAsmPrinter 
+LLVMNVPTXInfo LLVMPowerPCAsmPrinter LLVMPowerPCInfo LLVMPTXAsmPrinter 
+LLVMPTXInfo LLVMSparcInfo LLVMX86AsmPrinter LLVMX86Info LLVMMCParser 
+LLVMCodeGen LLVMXCoreInfo LLVMBitReader LLVMX86Utils LLVMScalarOpts 
+LLVMInstCombine LLVMTransformUtils LLVMipa LLVMAnalysis LLVMTarget 
+LLVMCore LLVMMC LLVMObject LLVMSupport clangRewrite clangFrontend 
+clangDriver clangSerialization clangParse clangSema clangAnalysis clangEdit 
+clangAST clangLex clangBasic LLVMMC LLVMObject LLVMSupport
+pthread
    )
   set_target_properties(${name} PROPERTIES FOLDER "Xclang executables")
 endmacro(add_xclang_executable)
