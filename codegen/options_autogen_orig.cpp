a6218a91b793f2f0d94d60f084f22a99f7b3add5
ma yingkui <maeikei@gmail.com>
2012-11-15 00:21:00 +0900

    merge

---
+++ codegen/options_autogen_orig.cpp
@@ -0,0 +1,30 @@
+#include <options.hpp>
+using namespace xclang;
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+#include <iostream>
+#include <iterator>
+using namespace std;
+void XClangOptions::parseArgs(void)
+{
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("xclang-target",value(&m_xclang_target) ,"xclang target ")
+            #{option_desc}
+        ;
+        variables_map vm;
+        store(parse_command_line(argc, argv, desc), vm);
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return;
+        }
+        #{vm_count}
+    }//try
+    catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+}
