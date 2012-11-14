93d6fdc0e18d3b276a520a8cd7d10ca114196205
ma yingkui <maeikei@gmail.com>
2012-11-15 00:23:57 +0900

    merge

---
+++ codegen/options_autogen_orig.hpp
@@ -0,0 +1,24 @@
+#ifndef __OPTIONS_HPP_
+#define __OPTIONS_HPP_
+
+#include <string>
+using namespace std;
+namespace xclang
+{
+    class XClangOptions
+    {
+    public:
+        XClangOptions(int argc,const char** argv);
+        ~XClangOptions();
+    private:
+        XClangOptions();
+    private:
+        const int m_argc;
+        const char** m_argv;
+    public:
+        #{header_members}
+    private:
+        #{header_methods}
+    };
+}
+#endif // __OPTIONS_HPP_
