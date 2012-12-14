3e178431ce3c3a1c200b6005d9c028763af63aef
ma yingkui <maeikei@gmail.com>
2012-12-14 09:04:39 +0900

    mingw link

--- test/gc.cpp
+++ test/gc.cpp
@@ -20,18 +20,18 @@ int __cxa_atexit ( void (*f)(void *), void *p, void *d )
 int
 __cxa_atexit (void (*func) (void *), void *arg, void *d)
 {
-    struct exit_function *new = __new_exitfn ();
+    struct exit_function *new_exit = __new_exitfn ();
     
-    if (new == NULL)
+    if (new_exit == NULL)
         return -1;
     
 #ifdef PTR_MANGLE
     PTR_MANGLE (func);
 #endif
-    new->func.cxa.fn = (void (*) (void *, int)) func;
-    new->func.cxa.arg = arg;
-    new->func.cxa.dso_handle = d;
+    new_exit->func.cxa.fn = (void (*) (void *, int)) func;
+    new_exit->func.cxa.arg = arg;
+    new_exit->func.cxa.dso_handle = d;
     atomic_write_barrier ();
-    new->flavor = ef_cxa;
+    new_exit->flavor = ef_cxa;
     return 0;
 }
