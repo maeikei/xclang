33dc5cee88c309fe6028344d6cb082843b6f8193
ma yingkui <maeikei@gmail.com>
2013-05-05 16:02:37 +0900

    change auto gen code

--- config/bin/bat/x86_64-w64-mingw32-xclang++.bat
+++
@@ -1,3 +0,0 @@
-@set pwd_path=%~dp0
-@set xclang_path=%pwd_path%xclang++.exe
-@"%xclang_path%" --xclang-target x86_64-w64-mingw32 %*
---
+++ config/bin/generate.rb
@@ -0,0 +1,32 @@
+$LUA_SCRIPT_HOME='../share'
+
+`find #{$LUA_SCRIPT_HOME} -type f -name *.lua`.split(/\n/).each do |arch|
+	prefix = File::basename(arch,'.lua')
+	if 'xclang-llvm' == prefix
+		next
+	end
+	
+run_shell = <<EOS
+#!/bin/bash
+xclang_path=<echo dollar0 | sed -e "s/#{prefix}-//">
+dollar(xclang_path) --xclang-target #{prefix} dollar*
+EOS
+	run_shell.gsub!(/</,"'")
+	run_shell.gsub!(/>/,"'")
+	run_shell.gsub!(/\(/,"{")
+	run_shell.gsub!(/\)/,"}")
+	run_shell.gsub!(/dollar/,"$")
+	open("shell/#{prefix}-clang","w") { |f| f.write run_shell }
+	open("shell/#{prefix}-clang++","w") { |f| f.write run_shell }
+
+rub_bat = <<EOS
+@set pwd_path=%~dp0
+@set xclang_path=%pwd_path%xclang.exe
+@"%xclang_path%" --xclang-target x86_64-pc-linux-gnueabi %*
+EOS
+        open("bat/#{prefix}-clang.bat","w") { |f| f.write rub_bat }
+        open("bat/#{prefix}-clang++.bat","w") { |f| f.write rub_bat }
+
+
+
+end
