fa856ddf450aa3351cf2380fffe2e5634f11ac41
ma yingkui <maeikei@gmail.com>
2013-05-06 16:36:56 +0900

    good build cxx static

--- 3rdpart_build/build-script/cxxrt/i386-linux-gnu.cmake
+++
@@ -1,7 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-SET (CMAKE_CROSSCOMPILING 1)
-set(ROOT_XCLANG /Users/maeikei/workspace/xclang-dev/obj/InstallRoot)
-
-set(CMAKE_C_COMPILER ${ROOT_XCLANG}/bin/i386-linux-gnu-xclang)
-set(CMAKE_CXX_COMPILER ${ROOT_XCLANG}/bin/i386-linux-gnu-xclang++)
-set(_CMAKE_TOOLCHAIN_PREFIX ${ROOT_XCLANG}/binutils/bin/i386-linux-gnu-)
