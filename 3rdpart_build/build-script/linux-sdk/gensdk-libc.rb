d64e142427ced282449245295cabff1a49e3a6ea
ma yingkui <maeikei@gmail.com>
2013-05-07 01:13:33 +0900

    cross

--- 3rdpart_build/build-script/linux-sdk/gensdk-libc.rb
+++ 3rdpart_build/build-script/linux-sdk/gensdk-libc.rb
@@ -4,32 +4,38 @@ $MIRROR = 'http://ftp.jaist.ac.jp/pub/Linux/ubuntu//pool'
 #http://ftp.jaist.ac.jp/pub/Linux/ubuntu//pool/main/l/linux/linux-libc-dev_3.8.0-19.29_amd64.deb
 #http://ftp.jaist.ac.jp/pub/Linux/ubuntu//pool/universe/a/armel-cross-toolchain-base/linux-libc-dev-armel-cross_3.8.0-5.10cross1.101_all.deb
 #http://ftp.jaist.ac.jp/pub/Linux/ubuntu//pool/main/e/eglibc/libc6_2.17-0ubuntu5_amd64.deb
+#http://ftp.jaist.ac.jp/pub/Linux/ubuntu//pool/main/g/gcc-4.7/libgcc-4.7-dev_4.7.3-1ubuntu1_amd64.deb
+#http://ftp.jaist.ac.jp/pub/Linux/ubuntu//pool/universe/g/gcc-4.7-armel-cross/libgcc-4.7-dev-armel-cross_4.7.3-1ubuntu1cross1.81_all.deb
 
 
 $PACKAGES =
 {
 	'x86_64'=>
 	[
+        ['main/g/gcc-4.7/','libgcc-4.7-dev_4.7.3-1ubuntu1_amd64.deb'],
         ['main/l/linux','linux-libc-dev_3.8.0-19.29_amd64.deb'],
-                ['main/e/eglibc','libc6_2.17-0ubuntu5_amd64.deb'],
+        ['main/e/eglibc','libc6_2.17-0ubuntu5_amd64.deb'],
 		['main/e/eglibc','libc6-dev_2.17-0ubuntu5_amd64.deb'],
 	],
 	'i386' =>
 	[
+        ['main/g/gcc-4.7/','libgcc-4.7-dev_4.7.3-1ubuntu1_i386.deb'],
         ['main/l/linux','linux-libc-dev_3.8.0-19.29_i386.deb'],
 		['main/e/eglibc','libc6_2.17-0ubuntu5_i386.deb'],
 		['main/e/eglibc','libc6-dev_2.17-0ubuntu5_i386.deb'],
 	],
     'armel' =>
     [
+        ['universe/g/gcc-4.7-armel-cross','libgcc-4.7-dev-armel-cross_4.7.3-1ubuntu1cross1.81_all.deb'],
         ['universe/a/armel-cross-toolchain-base','linux-libc-dev-armel-cross_3.8.0-5.10cross1.101_all.deb'],
-	['universe/a/armel-cross-toolchain-base','libc6-armel-cross_2.17-0ubuntu2cross1.101_all.deb'],
+        ['universe/a/armel-cross-toolchain-base','libc6-armel-cross_2.17-0ubuntu2cross1.101_all.deb'],
         ['universe/a/armel-cross-toolchain-base','libc6-dev-armel-cross_2.17-0ubuntu2cross1.101_all.deb'],
     ],
     'armhf' =>
     [
+        ['universe/g/gcc-4.7-armhf-cross','libgcc-4.7-dev-armhf-cross_4.7.3-1ubuntu1cross1.81_all.deb'],
         ['universe/a/armhf-cross-toolchain-base','linux-libc-dev-armhf-cross_3.8.0-5.10cross1.101_all.deb'],
-	['universe/a/armhf-cross-toolchain-base','libc6-armhf-cross_2.17-0ubuntu2cross1.101_all.deb'],
+        ['universe/a/armhf-cross-toolchain-base','libc6-armhf-cross_2.17-0ubuntu2cross1.101_all.deb'],
         ['universe/a/armhf-cross-toolchain-base','libc6-dev-armhf-cross_2.17-0ubuntu2cross1.101_all.deb'],
     ],
 }
